8a97baa2f45a7524682246175350d08094fcd452
Emmanuel Ogebe <55029744+emmanuelogebe@users.noreply.github.com>
2021-12-09 15:22:19 +0100

    Update app.py

--- Scraping Books/app.py
+++ Scraping Books/app.py
@@ -9,3 +9,9 @@ books = page.books
 
 for book in books:
     print(book)
+
+for page_num in range(1, 50):
+    website = f"http://books.toscrape.com/catalogue/page-{page_num+1}.html"
+    page_content = requests.get(website).content
+    page = Catalogue(page_content)
+    books.extend(page.books)
