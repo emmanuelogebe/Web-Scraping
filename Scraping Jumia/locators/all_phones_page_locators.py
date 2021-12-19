dd6da8f1ffa67655f4acdc401b01fc6bed12a359
Emmanuel Ogebe <55029744+emmanuelogebe@users.noreply.github.com>
2021-12-19 18:00:58 +0100

    Update and rename all_phones_page_locators.py to phone_locators.py

--- Scraping Jumia/locators/all_phones_page_locators.py
+++
@@ -1,3 +0,0 @@
-class AllPhonesPageLocators:
-    PHONES = 'div.page_inner section article.prd._fb.col.c-prd'
-
---
+++ Scraping Jumia/locators/phone_locators.py
@@ -0,0 +1,5 @@
+class PhoneLocators:
+    Name_Locator = 'a.core info h3.name'
+    Current_Price_Locator = 'a.core info div.prc'
+    Old_Price_Locator = 'a.core info div.s-prc-w div.old'
+    Percentage_Reduction = 'a.core info div.s-prc-w div.tag._dsct._sm'
